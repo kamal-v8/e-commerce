4f994ba20a1c8794a113755df875a61c0691a202
kamal <kamal.v3011f@gmail.com>
2026-03-20 17:58:31 +0530

    moved to zaptor.in

--- brew-and-blooom/terraform/variable.tf
+++ brew-and-blooom/terraform/variable.tf
@@ -1,7 +1,8 @@
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
+  # default     = "t3.micro"
+  default = "c7i-flex.large"
 }
 
 #  variable "my_ip" {
